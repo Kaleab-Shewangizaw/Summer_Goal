dc2b0e93c7d6ff63ebbc7ee15be3d18121e44456
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-05-22 17:43:22 +0300

    secure: true, sameSite: 'None'

--- backend/controllers/auth..controller.js
+++ backend/controllers/auth..controller.js
@@ -120,8 +120,8 @@ export async function login(req, res) {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "strict",
+        secure: true,
+        sameSite: "None",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       })
       .json({
